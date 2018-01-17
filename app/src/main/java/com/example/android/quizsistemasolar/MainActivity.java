04e0f49d4dc0eb990ed1ad8f9fdf72383cc41234
S <hartmnn.p@gmail.com>
2018-01-17 16:56:30 -0200

    Code Review v1

--- app/src/main/java/com/example/android/quizsistemasolar/MainActivity.java
+++ app/src/main/java/com/example/android/quizsistemasolar/MainActivity.java
@@ -14,8 +14,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    double score = 0;
-    String userAnswer;
+    private int score;
+    private String userAnswer;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
@@ -29,98 +29,95 @@ public class MainActivity extends AppCompatActivity {
     public void askRadioButton() {
 
         //Answer for Question 1
-        RadioButton answer1_ask1 = findViewById(R.id.answer1_ask1);
-        boolean banswer1_ask1 = answer1_ask1.isChecked( );
-        RadioButton answer2_ask1 = findViewById(R.id.answer2_ask1);
-        boolean banswer2_ask1 = answer2_ask1.isChecked( );
-        RadioButton answer3_ask1 = findViewById(R.id.answer3_ask1);
-        boolean banswer3_ask1 = answer3_ask1.isChecked( );
+        RadioButton answer1Ask1 = findViewById(R.id.answer1Ask1);
+        boolean bAnswer1Ask1 = answer1Ask1.isChecked( );
+        RadioButton answer2Ask1 = findViewById(R.id.answer2Ask1);
+        boolean bAnswer2Ask1 = answer2Ask1.isChecked( );
+        RadioButton answer3Ask1 = findViewById(R.id.answer3Ask1);
+        boolean bAnswer3Ask1 = answer3Ask1.isChecked( );
 
-        if (banswer1_ask1) score += 1;
-        if (banswer2_ask1) score -= 1;
-        if (banswer3_ask1) score -= 1;
+        if (bAnswer1Ask1) score += 1;
+        if (bAnswer2Ask1) score -= 1;
+        if (bAnswer3Ask1) score -= 1;
 
         //Answer for Question 4
-        RadioButton answer1_ask4 = findViewById(R.id.answer1_ask4);
-        boolean banswer1_ask4 = answer1_ask4.isChecked( );
-        RadioButton answer2_ask4 = findViewById(R.id.answer2_ask4);
-        boolean banswer2_ask4 = answer2_ask4.isChecked( );
-        RadioButton answer3_ask4 = findViewById(R.id.answer3_ask4);
-        boolean banswer3_ask4 = answer3_ask4.isChecked( );
+        RadioButton answer1Ask4 = findViewById(R.id.answer1Ask4);
+        boolean bAnswer1Ask4 = answer1Ask4.isChecked( );
+        RadioButton answer2Ask4 = findViewById(R.id.answer2Ask4);
+        boolean bAnswer2Ask4 = answer2Ask4.isChecked( );
+        RadioButton answer3Ask4 = findViewById(R.id.answer3Ask4);
+        boolean bAnswer3Ask4 = answer3Ask4.isChecked( );
 
-        if (banswer1_ask4) score += 1;
-        if (banswer2_ask4) score -= 1;
-        if (banswer3_ask4) score -= 1;
+        if (bAnswer1Ask4) score += 1;
+        if (bAnswer2Ask4) score -= 1;
+        if (bAnswer3Ask4) score -= 1;
 
         //Answer for Question 5
-        RadioButton answer1_ask5 = findViewById(R.id.answer1_ask5);
-        boolean banswer1_ask5 = answer1_ask5.isChecked( );
-        RadioButton answer2_ask5 = findViewById(R.id.answer2_ask5);
-        boolean banswer2_ask5 = answer2_ask5.isChecked( );
+        RadioButton answer1Ask5 = findViewById(R.id.answer1Ask5);
+        boolean bAnswer1Ask5 = answer1Ask5.isChecked( );
+        RadioButton answer2Ask5 = findViewById(R.id.answer2Ask5);
+        boolean bAnswer2Ask5 = answer2Ask5.isChecked( );
 
-        if (banswer1_ask5) score += 1;
-        if (banswer2_ask5) score -= 1;
+        if (bAnswer1Ask5) score += 1;
+        if (bAnswer2Ask5) score -= 1;
 
         //Answer for Question 6
-        RadioButton answer1_ask6 = findViewById(R.id.answer1_ask6);
-        boolean banswer1_ask6 = answer1_ask6.isChecked( );
-        RadioButton answer2_ask6 = findViewById(R.id.answer2_ask6);
-        boolean banswer2_ask6 = answer2_ask6.isChecked( );
+        RadioButton answer1Ask6 = findViewById(R.id.answer1Ask6);
+        boolean bAnswer1Ask6 = answer1Ask6.isChecked( );
+        RadioButton answer2Ask6 = findViewById(R.id.answer2Ask6);
+        boolean bAnswer2Ask6 = answer2Ask6.isChecked( );
 
-
-        if (banswer1_ask6) score += 1;
-        if (banswer2_ask6) score -= 1;
-
+        if (bAnswer1Ask6) score += 1;
+        if (bAnswer2Ask6) score += 1;
     }
 
     /**
      * CheckBox Question 2
      */
     public void askCheckBox() {
-        CheckBox checkAnswer1 = findViewById(R.id.answer1_ask2);
-        CheckBox checkAnswer2 = findViewById(R.id.answer2_ask2);
-        CheckBox checkAnswer3 = findViewById(R.id.answer3_ask2);
+        CheckBox checkAnswer1 = findViewById(R.id.answer1Ask2);
+        CheckBox checkAnswer2 = findViewById(R.id.answer2Ask2);
+        CheckBox checkAnswer3 = findViewById(R.id.answer3Ask2);
 
         boolean checked1 = checkAnswer1.isChecked( );
         boolean checked2 = checkAnswer2.isChecked( );
         boolean checked3 = checkAnswer3.isChecked( );
 
+        if (checked2 && checked3) score += 1;
         if (checked1) score -= 1;
-        if (checked2) score += 0.5;
-        if (checked3) score += 0.5;
-
     }
 
     /**
      * EditBox Question 3
      */
     public void askEditBox() {
-        EditText answer = findViewById(R.id.answer1_ask3);
+        EditText answer = findViewById(R.id.answer1Ask3);
         userAnswer = String.valueOf(answer.getText( ));
-
-        if (userAnswer.equals("Láctea")) {
-            score += 1;
-
-        } else if (userAnswer.equals("Via Láctea")) {
-            score += 1;
-
-        } else {
-            score -= 1;
-
+            if (userAnswer.equalsIgnoreCase("Láctea") || userAnswer.equalsIgnoreCase("Via Láctea") || userAnswer.equalsIgnoreCase("Via Lactea") || userAnswer.equalsIgnoreCase("Lactea")) score += 1;
+            else if (userAnswer.equalsIgnoreCase("")) score += 0;
+            else score -= 1;
         }
 
-    }
 
     public void rate( View view ) {
         // Call and Calc score
         askEditBox( );
         askCheckBox( );
         askRadioButton( );
+        // Toast.makeText(this, String.valueOf(score), Toast.LENGTH_SHORT).show( );
 
-        if (score > 5.9)
-            Toast.makeText(this, (getString(R.string.maxScore)), Toast.LENGTH_SHORT).show( );
-        else Toast.makeText(this, (getString(R.string.minScore)), Toast.LENGTH_SHORT).show( );
-
+        if (score >= 5)
+            Toast.makeText(this, getString(R.string.maxScore), Toast.LENGTH_SHORT).show( );
+        else if (score == 4)
+            Toast.makeText(this, getString(R.string.scoreEquals4), Toast.LENGTH_SHORT).show( );
+        else if (score == 3)
+            Toast.makeText(this, getString(R.string.scoreEquals3), Toast.LENGTH_SHORT).show( );
+        else if (score == 2)
+            Toast.makeText(this, getString(R.string.scoreEquals2), Toast.LENGTH_SHORT).show( );
+        else if (score == 1)
+            Toast.makeText(this, getString(R.string.scoreEquals1), Toast.LENGTH_SHORT).show( );
+        else
+            Toast.makeText(this, getString(R.string.minScore), Toast.LENGTH_SHORT).show( );
         //Retuns to 0 score value
         score = 0;
     }
@@ -130,92 +127,8 @@ public class MainActivity extends AppCompatActivity {
 
         Uri webpage = Uri.parse("https://www.jw.org/pt/publicacoes/livros/vida-teve-criador/");
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-        if (intent.resolveActivity(getPackageManager()) != null) {
+        if (intent.resolveActivity(getPackageManager( )) != null) {
             startActivity(intent);
         }
     }
-
-
 }
-
-    /**
-     *
-     *
-     * Aqui estão algumas tentativas que eu fiz e não consegui terminar. Não sei por que não deu certo.
-     * Se você puder em explicar melhor eu agradeceria muito! hartmnn.p@gmail.com
-    * */
-
-/*
-    // Primeira Tentativa com base na Documentação Android RadioButton
-    // https://developer.android.com/guide/topics/ui/controls/radiobutton.html
-
-    double rating = 0;
-
-    @Override
-    protected void onCreate( Bundle savedInstanceState ) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void askRadioButton(View view){
-        //is checked?
-        boolean checked = ((RadioButton)view).isChecked();
-
-        //Do it now!!!!
-        switch (view.getId()){
-
-            case R.id.answer1_ask1:
-                if (checked) {
-                    rating += 1;
-                    break;
-                }
-            case R.id.answer1_ask2:
-                if (checked) rating -= 1;
-                break;
-
-            case R.id.answer2_ask2:
-                if (checked) rating += 0.5;
-                break;
-
-            case R.id.answer3_ask2:
-                if (checked) rating += 0.5;
-                break;
-
-            case R.id.answer1_ask4:
-                if (checked) {
-                    rating += 1;
-                    break;
-                }
-            case R.id.answer2_ask4:
-                if (checked) {
-                    rating -= 1;
-                    break;
-                }
-            case R.id.answer3_ask4:
-                if (checked) {
-                    rating -= 1;
-                    break;
-                }
-            case R.id.answer1_ask5:
-                if(checked){
-                    rating += 1;
-                }
-            case R.id.answer2_ask5:
-                if (checked) {
-                    rating -= 1;
-                }
-        }
-    }
-
-    public void rate( View view ) {
-        int radioButtonID = radioButtonGroup.getCheckedRadioButtonId();
-        View radioButton = radioButtonGroup.findViewById(radioButtonID);
-
-        askRadioButton(radioButton);
-
-        Toast.makeText(this, "Teste pontuação " + rating ), Toast.LENGTH_SHORT).show();
-        rating = 0;
-    }
-}
-
-*/
